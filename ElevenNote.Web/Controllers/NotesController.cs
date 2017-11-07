83d1179e88e60f24dcc607e1d3e6a53d01b26fc7
tpageforfunzies <tjpage@indiana.edu>
2017-11-07 12:53:58 -0500

    made notes controller

---
+++ ElevenNote.Web/Controllers/NotesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.Web.Controllers
+{
+    public class NotesController : Controller
+    {
+        // GET: Notes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
