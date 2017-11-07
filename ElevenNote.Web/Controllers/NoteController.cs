ea7a04b1f0cce2fb42f15be5315bb320a5d7f95c
tpageforfunzies <tjpage@indiana.edu>
2017-11-07 13:21:28 -0500

    end of module 5

---
+++ ElevenNote.Web/Controllers/NoteController.cs
@@ -0,0 +1,35 @@
+﻿using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.Web.Controllers
+{
+    public class NotesController : Controller
+    {
+        // GET: Notes
+        public ActionResult Index()
+        {
+            var model = new NoteListItem[0];
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
+        }
+    }
+}
